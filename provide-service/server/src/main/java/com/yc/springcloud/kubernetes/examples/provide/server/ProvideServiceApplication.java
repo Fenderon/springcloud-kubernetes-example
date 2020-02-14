96a23b4c95532ec85352c643731b1521c2eefc46
unknown <842317348@qq.com>
2020-02-14 10:51:46 +0800

    添加本地catalog-services配置，关闭k8s定时查询服务

--- provide-service/server/src/main/java/com/yc/springcloud/kubernetes/examples/provide/server/ProvideServiceApplication.java
+++ provide-service/server/src/main/java/com/yc/springcloud/kubernetes/examples/provide/server/ProvideServiceApplication.java
@@ -21,6 +21,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -34,6 +36,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableSwagger2
+@EnableAsync
+@EnableScheduling
 @ComponentScan(basePackages = {"com.yc.springcloud.kubernetes.examples"})
 public class ProvideServiceApplication {
 
