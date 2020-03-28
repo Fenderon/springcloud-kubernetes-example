1bb0ff53b3ceb1bf57c53796f0c284450a17b1a4
yangchuan <842317348@qq.com>
2020-03-28 21:58:54 +0800

    update deploy

--- consumer-service/deploy.sh
+++ consumer-service/deploy.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
+workdir=$(cd "$(dirname "$0")"; pwd)
+echo workdir
 #这里会同时构建client、common模块
-mvn clean install -f pom.xml -pl client -am
-mvn clean package -pl server -Dmaven.test.skip=true -Dtag.version=dev.latest
-mvn dockerfile:push -pl server -Dtag.version=dev.latest
-kubectl delete --ignore-not-found=true -f ./server/src/main/resources/consumer-dev.yaml
-kubectl apply -f ./server/src/main/resources/consumer-dev.yaml
+mvn clean install -f ${workdir}/pom.xml -pl client -am
+mvn clean package -f ${workdir}/pom.xml -pl server -Dmaven.test.skip=true -Dtag.version=dev.latest
+mvn dockerfile:push -f ${workdir}/pom.xml -pl server -Dtag.version=dev.latest
+kubectl delete --ignore-not-found=true -f ${workdir}/server/src/main/resources/consumer-dev.yaml
+kubectl apply -f ${workdir}/server/src/main/resources/consumer-dev.yaml
 
