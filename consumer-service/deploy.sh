a2aea120f4fd7e7cdb576649622c282c9401d8fd
yangchuan <842317348@qq.com>
2020-03-28 22:04:50 +0800

    update deploy

--- consumer-service/deploy.sh
+++ consumer-service/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 workdir=$(cd "$(dirname "$0")"; pwd)
-echo workdir
+echo ${workdir}
 #这里会同时构建client、common模块
 mvn clean install -f ${workdir}/pom.xml -pl client -am
 mvn clean package -f ${workdir}/pom.xml -pl server -Dmaven.test.skip=true -Dtag.version=dev.latest
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 #mvn install -f pom.xml
-mvn clean install -f service-parent/pom.xml
-mvn clean clean pacakge -f pom.xml -pl comsumer-service -am -Dmaven.test.skip=true -Dtag.version=dev.latest
-mvn clean install -f pom.xml -pl comsumer-service -Dmaven.test.skip=true -Dtag.version=dev.latest
-./service-parent/deploy.sh
-./consumer-service/deploy.sh
-./provide-service/deploy.sh
-./gateway-service/deploy.sh
-
-mvn clean install -f pom.xml -pl comsumer-service -am
+sh ./service-parent/deploy.sh
+sh ./service-rpc/deploy.sh
+sh ./service-common/deploy.sh
+sh ./module-cache/deploy.sh
+sh ./module-field-comparator//deploy.sh
+sh ./consumer-service/deploy.sh
+sh ./provide-service/deploy.sh
+sh ./gateway-service/deploy.sh
